3bcd76917c5325226bf7988bf71a4eacb2ad550a
guochenyang <630974147@qq.com>
2017-03-13 02:18:00 +0800

    OJ test

--- OJ/1001/1001.c
+++ OJ/1001/1001.c
@@ -3,23 +3,39 @@
 #include<stdlib.h>
 
 #define INPUT_LEN 1000
+int mallocMem(char** str, unsigned int len,int mallocId)
+{
+    char *tmpStr=NULL;
+    tmpStr = (char*)malloc(len * sizeof(char));
+    if(NULL == tmpStr)
+    {
+        printf("malloc mem failed, mallocId:%d.\n", mallocId);
+        return;
+    }
+    *str =tmpStr;
+}
+void CalculateX_2(char *str)
+{
+    
+}
+
 int main(void)
 {
     char *str=NULL;
-    int n=0;
-
-    str = (char*)malloc(INPUT_LEN * sizeof(char));
-    if(NULL == str)
-    {
-        printf("malloc mem failed.\n");
-        return;
-    }
-
-
-
-    while(scanf("%s%d", str, &n)!=EOF)
+    char *result=NULL;
+    unsigned int n=0;
+    mallocMem(&str,INPUT_LEN,0);
+    mallocMem(&result, INPUT_LEN, 1);
+       //input 
+    while(scanf("%s%u", str, &n)!=EOF)
     {
         printf("%s %d\n", str, n);
+        while(--n)
+        {
+            CalculateX_2(str);
+        }        
+        memcpy(result,str,sizeof(str));
+        printf("%s\n",result);
     }    
 
 
