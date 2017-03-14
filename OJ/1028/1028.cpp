8484bab1484426ee962803c56de9070576ca3f53
guochenyang <630974147@qq.com>
2017-03-15 02:13:29 +0800

    poj 1028

--- OJ/1028/1028.cpp
+++ OJ/1028/1028.cpp
@@ -7,9 +7,10 @@ using namespace std;
 
 typedef unsigned int uint32_t;
 
+
 int main()
 {
-    string ori_str = "http://www.acm.org/";
+    string cur_str = "http://www.acm.org/";
     string line_data;
     string cmd;
     string real_data;
@@ -17,27 +18,62 @@ int main()
     stack<string> forward_stack;
     stack<string> backward_stack;
 
-    backward_stack.push(ori_str);
-    
+
     while(getline(cin,line_data))
     {
-       ss.str(line_data);
-       ss>>cmd;
-       //cout<<cmd<<endl; 
-       if(cmd == "EXIT")
+        ss.str("");
+        ss.clear();
+        //cout<<"line_data:"<<line_data<<endl;
+        ss.str(line_data);
+        ss>>cmd;
+        //cout<<cmd<<endl; 
+        if(cmd == "QUIT")
         {
             //cout<<"exit";
             return 0;
         }
         else if("VISIT" == cmd )
         {   
-           ss>>real_data;
-            backward_stack.push(real_data);
-            
+            ss>>real_data;
+            backward_stack.push(cur_str);
+            while(!forward_stack.empty())
+            {
+                forward_stack.pop();
+            }   
+            cur_str = real_data; 
+            cout<<cur_str<<endl;
         }
-        
+        else if("BACK" == cmd)
+        {
+            if(backward_stack.empty())
+            {
+                cout<<"Ignored"<<endl;
+                continue;
+            }
+            forward_stack.push(cur_str);
+            cur_str = backward_stack.top();
+            backward_stack.pop();
+            cout<<cur_str<<endl;
+        }
+        else if("FORWARD" == cmd)
+        {
+            if(forward_stack.empty())
+            {
+                cout<<"Ignored"<<endl;
+                continue;
+            }
+            backward_stack.push(cur_str);
+            cur_str = forward_stack.top();
+            forward_stack.pop();
+            cout<<cur_str<<endl;
+        }
+        else
+        {
+            cout<<"error."<<endl;
+        }
+
     }
-    
+
     return 0;
 }
 
